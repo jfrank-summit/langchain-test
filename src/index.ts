2a6ac4977a4f0abaa81828cd51e22e6db72d3148
Jeremy Frank <37932802+jfrank-summit@users.noreply.github.com>
2023-04-30 12:22:14 +0000

    initial langchain setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
+import * as dotenv from "dotenv";
+import { OpenAI } from "langchain/llms/openai";
 
+dotenv.config();
+
+const model = new OpenAI({
+    modelName: "gpt-3.5-turbo",
+    openAIApiKey: process.env.OPENAI_API_KEY,
+});
 
 const main = async () => {
-    console.log("hello world")
-}
+    const res = await model.call("What's a good idea for an application to build with GPT-3?");
+    console.log(res);
+};
 
 main().catch(console.error);

08c6718385cd03ea031b021c025138ae9839ee7e
Jeremy Frank <jeremy@subspace.network>
2023-05-28 15:30:52 +0400

    add memoryvectorstore

--- src/chat.ts
+++ src/chat.ts
@@ -12,12 +12,15 @@ import {
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
     MessagesPlaceholder,
+    PromptTemplate,
     SystemMessagePromptTemplate,
 } from "langchain/prompts";
 import { ConversationChain } from "langchain/chains";
 import { BufferMemory, BufferWindowMemory } from "langchain/memory";
 import * as readline from 'readline';
 import { VectorStoreRetrieverMemory, ENTITY_MEMORY_CONVERSATION_TEMPLATE, EntityMemory } from "langchain/memory";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
 
 dotenv.config();
 
@@ -27,16 +30,28 @@ const initChain = async () => {
         openAIApiKey: process.env.OPENAI_API_KEY,
         modelName: "gpt-3.5-turbo",
     });
+    const vectorStore = new MemoryVectorStore(new OpenAIEmbeddings());
+    const memory = new VectorStoreRetrieverMemory({
+        // 1 is how many documents to return, you might want to return more, eg. 4
+        vectorStoreRetriever: vectorStore.asRetriever(1),
+        memoryKey: "history",
+    });
+    const chatPrompt =  PromptTemplate.fromTemplate(`The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
 
-    const chatPrompt = ChatPromptTemplate.fromPromptMessages([
-        SystemMessagePromptTemplate.fromTemplate(generateInitializationPrompt()),
-        new MessagesPlaceholder("history"),
-        HumanMessagePromptTemplate.fromTemplate("{input}"),
-    ]);
+        Relevant pieces of previous conversation:
+        {history}
+
+        (You do not need to use these pieces of information if not relevant)
+
+        Current conversation:
+        Human: {input}
+        AI:`);
+
     const chain = new ConversationChain({
-        memory: new BufferWindowMemory({ returnMessages: true, memoryKey: "history", k:10 }),
+        memory,
         prompt: chatPrompt,
         llm: chat,
+        verbose: true,
     });
     return chain;
 };
@@ -65,11 +80,11 @@ const main = async () => {
         const input = await question("What action would you like to take? ");
         const action = await chain.call({ input });
         console.log(`action: ${JSON.stringify(action)}`);
-        const reflection = await chain.call({ input: generateReflectionPrompt() });
-        if (reflection.response === "done") {
-            rl.close();
-            return;
-        }
+        //const reflection = await chain.call({ input: generateReflectionPrompt() });
+        // if (reflection.response === "done") {
+        //     rl.close();
+        //     return;
+        // }
        
     }
 };
